b92fd132c6b15da5b494036364ecf22b418e6773
thilina-fer <dilshanthilina1010@gmail.com>
2026-02-12 21:30:06 +0530

    feat : create a itemDTO class

---
+++ Backend/springboot-POS-Backend/src/main/java/com/example/springbootposbackend/dto/ItemDTO.java
@@ -0,0 +1,28 @@
+package com.example.springbootposbackend.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+
+public class ItemDTO {
+
+    @Pattern(regexp = "^\\d+$", message = "Item ID must be a valid integer")
+    private int id;
+
+    @NotBlank(message = "Item description cannot be blank")
+    private String description;
+
+    @NotBlank(message = "Item price cannot be blank")
+    @Pattern(regexp = "[+-]?(\\d+(\\.\\d*)?|\\.\\d+)([eE][+-]?\\d+)?" , message = "Invalid unit price")
+    private double unitPrice;
+
+    @Pattern(regexp = "^\\d+$", message = "Item qty must be a valid integer")
+    @NotBlank(message = "Item qty cannot be blank")
+    private int qtyOnHand;
+}
